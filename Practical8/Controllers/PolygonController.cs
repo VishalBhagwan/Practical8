c43f3294ebb652cc7b122082610c5eb739328caf
Vishal Bhagwan <u23684722@tuks.co.za>
2025-06-02 23:59:36 +0200

    Views

---
+++ Practical8/Controllers/PolygonController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practical8.Controllers
+{
+    public class PolygonController : Controller
+    {
+        // GET: Polygon
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SelectShape()
+        {
+            return View();
+        }
+
+        public ActionResult Circle()
+        {
+            return View();
+        }
+
+        public ActionResult Rectangle()
+        {
+            return View();
+        }
+
+        public ActionResult Ellipse()
+        {
+            return View();
+        }
+
+        public ActionResult Triangle()
+        {
+            return View();
+        }
+    }
+}
